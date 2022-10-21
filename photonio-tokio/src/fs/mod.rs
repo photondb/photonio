58fa9c0bcc4003386b2a078a6c1eec9e2aa7473c
Richard <huachao.huang@gmail.com>
2022-10-21 11:28:48 +0800

    Add some missing fs functions (#3)

--- photonio-tokio/src/fs/mod.rs
+++ photonio-tokio/src/fs/mod.rs
@@ -1,3 +1,5 @@
+use std::{io::Result, path::Path};
+
 mod open;
 pub use open::OpenOptions;
 
@@ -6,3 +8,19 @@ pub use file::File;
 
 mod metadata;
 pub use metadata::Metadata;
+
+pub async fn rename<P: AsRef<Path>, Q: AsRef<Path>>(from: P, to: Q) -> Result<()> {
+    tokio::fs::rename(from, to).await
+}
+
+pub async fn remove_file<P: AsRef<Path>>(path: P) -> Result<()> {
+    tokio::fs::remove_file(path).await
+}
+
+pub async fn create_dir<P: AsRef<Path>>(path: P) -> Result<()> {
+    tokio::fs::create_dir(path).await
+}
+
+pub async fn remove_dir<P: AsRef<Path>>(path: P) -> Result<()> {
+    tokio::fs::remove_dir(path).await
+}
